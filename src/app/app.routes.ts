36b1f5dbd2383252556707e70db57c6ce1286167
P4bloFyre <ptrigor@alumnos.ceduc.cl>
2024-12-03 21:22:27 -0300

    Archivos Prueba

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent},          // Ruta para Inicio
+  { path: 'catalogo', component: CatalogoComponent }, // Ruta para Catálogo
+  { path: 'contacto', component: ContactoComponent }, // Ruta para Contacto
+  { path: 'acerca-de', component: AcercaDeComponent } // Ruta para Acerca de
+];
